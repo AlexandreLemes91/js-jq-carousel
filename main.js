be248d6078cd99c3089a3338885463738c058dd6
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-04-08 20:34:24 +0200

    end

--- main.js
+++ main.js
@@ -9,6 +9,18 @@ prevBtn.click( function(){
     traslazione("prev");
 })
 
+//KeyBoard
+$(document).keydown(function(t){
+    console.log(t.keyCode);
+
+    if(t.keyCode === 37){
+        traslazione("prev");
+    }else if(t.keyCode === 39){
+        traslazione("next");
+    }
+})
+
+
 /* FUNCTION */
 function traslazione(direzione){
     var imgActive = $("img.active");
@@ -18,18 +30,21 @@ function traslazione(direzione){
     circleActive.removeClass("active");
 
     if( direzione === "next"){
-        if(imgActive.hasClass("last")){
+        if(imgActive.hasClass("last")){ //e se facessi qualcosa tipo imgActive === null per riconoscere l ultimo?
             $("img.first").addClass("active");
             $(".nav i.first").addClass("active");
+        }else{
+            imgActive.next("img").addClass("active"); 
+            circleActive.next("i").addClass("active");
         }
-        imgActive.next("img").addClass("active");
-        circleActive.next("i").addClass("active");
-    }else if( direzione === "prev" ){
+    }
+    if( direzione === "prev" ){
         if(imgActive.hasClass("first")){
             $("img.last").addClass("active");
             $(".nav i.last").addClass("active");
+        }else{
+            imgActive.prev("img").addClass("active");
+            circleActive.prev("i").addClass("active");
         }
-        imgActive.prev("img").addClass("active");
-        circleActive.prev("i").addClass("active");
     }
 }
